59084a63b1697dd6374b13c8e362f39f4df93a55
FacuTombesi <facundomtombesi@gmail.com>
2025-03-27 22:06:44 -0300

    python frontend basic

--- frontend_python/frontend_python.py
+++ frontend_python/frontend_python.py
@@ -6,7 +6,17 @@ class State(rx.State):
 
 # Function that renders the components of the page
 def index() -> rx.Component:
-    return rx.text("Hello world!")
+    return rx.hstack(
+        rx.text(
+            "<ftombesi/>",
+            height="40px"
+        ),
+        position="sticky",
+        bg="purple",
+        padding_x="16px",
+        padding_y="4px",
+        z_index="999"
+    )
 
 app = rx.App()
 app.add_page(index) # Add the page created above
