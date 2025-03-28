f2df5bb16e1c8d9307e21b351798afb34066076f
FacuTombesi <facundomtombesi@gmail.com>
2025-03-28 17:30:23 -0300

    python frontend basic

--- frontend_python/views/links/links.py
+++ frontend_python/views/links/links.py
@@ -2,7 +2,32 @@ import reflex as rx
 
 def links() -> rx.Component:
   return rx.vstack(
-    rx.avatar(fallback="FT", color_scheme="crimson", size="8"),
-    rx.text("Mini-Projects"),
-    rx.text("Welcome to the Mini-projects hub.")
+    rx.button(
+      "Calculator",
+      size="3",
+      variant="solid",
+      color_scheme="gray",
+      radius="small"
+    ),
+    rx.button(
+      "Snake Game",
+      size="3",
+      variant="solid",
+      color_scheme="grass",
+      radius="small"
+    ),
+    rx.button(
+      "Weather App",
+      size="3",
+      variant="solid",
+      color_scheme="crimson",
+      radius="small"
+    ),
+    rx.button(
+      "Budget Tracker",
+      size="3",
+      variant="solid",
+      color_scheme="purple",
+      radius="small"
+    ),
   )
