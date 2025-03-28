6833af1ed1db73a2067cfd0e13bcae47efd0f083
FacuTombesi <facundomtombesi@gmail.com>
2025-03-28 17:22:45 -0300

    python frontend basic

--- frontend_python/views/links/links.py
+++ frontend_python/views/links/links.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
-def header() -> rx.Component:
+def links() -> rx.Component:
   return rx.vstack(
     rx.avatar(fallback="FT", color_scheme="crimson", size="8"),
     rx.text("Mini-Projects"),
