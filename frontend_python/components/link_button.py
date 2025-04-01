e1a43cdf1b0f8145ac6f2d59cbff2492fde02d43
FacuTombesi <facundomtombesi@gmail.com>
2025-03-31 21:17:10 -0300

    python frontend basic

--- frontend_python/components/link_button.py
+++ frontend_python/components/link_button.py
@@ -1,10 +1,13 @@
 import reflex as rx
 
-def link_button(text: str, color: str) -> rx.Component:
-  return rx.button(
-    text,
-    size="3",
-    variant="solid",
-    color_scheme=color,
-    radius="small"
+def link_button(text: str, color: str, url: str) -> rx.Component:
+  return rx.link(
+    rx.button(
+      text,
+      size="3",
+      variant="solid",
+      color_scheme=color,
+      radius="small"
+    ),
+    href=url
   )
--- frontend_python/views/links/links.py
+++ frontend_python/views/links/links.py
@@ -3,8 +3,8 @@ from frontend_python.components.link_button import link_button
 
 def links() -> rx.Component:
   return rx.vstack(
-    link_button("Calculator", "gray"),
-    link_button("Snake Game", "grass"),
-    link_button("Weather App", "crimson"),
-    link_button("Budget Tracker", "purple")
+    link_button("Calculator", "gray", "https://ftcalculator.vercel.app/"),
+    link_button("Snake Game", "grass", "https://ftsnakegame.vercel.app/"),
+    link_button("Weather App", "crimson", "https://ftweatherapp.vercel.app/"),
+    link_button("Budget Tracker", "purple", "https://ftbudgettracker.vercel.app/")
   )
