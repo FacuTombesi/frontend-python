44e053b39b7b70d448db3f9a1fec512677258d31
FacuTombesi <facundomtombesi@gmail.com>
2025-04-03 17:46:07 -0300

    python frontend basic

--- frontend_python/views/links/links.py
+++ frontend_python/views/links/links.py
@@ -2,9 +2,11 @@ import reflex as rx
 from frontend_python.components.link_button import link_button
 
 def links() -> rx.Component:
-  return rx.vstack(
+  return rx.stack(
     link_button("Calculator", "gray", "https://ftcalculator.vercel.app/"),
     link_button("Snake Game", "grass", "https://ftsnakegame.vercel.app/"),
     link_button("Weather App", "crimson", "https://ftweatherapp.vercel.app/"),
-    link_button("Budget Tracker", "purple", "https://ftbudgettracker.vercel.app/")
+    link_button("Budget Tracker", "purple", "https://ftbudgettracker.vercel.app/"),
+    direction="column",
+    align="center"
   )
