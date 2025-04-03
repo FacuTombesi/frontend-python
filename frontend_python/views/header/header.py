ce62c09945661e15d2bfd09d58b1bcc7d48f83f0
FacuTombesi <facundomtombesi@gmail.com>
2025-04-03 17:46:20 -0300

    python frontend basic

--- frontend_python/views/header/header.py
+++ frontend_python/views/header/header.py
@@ -1,8 +1,10 @@
 import reflex as rx
 
 def header() -> rx.Component:
-  return rx.vstack(
+  return rx.stack(
     rx.avatar(fallback="FT", color_scheme="crimson", size="8"),
     rx.text("Mini-Projects"),
-    rx.text("Welcome to the Mini-projects hub.")
+    rx.text("Welcome to the Mini-projects hub."),
+    direction="column",
+    align="center"
   )
