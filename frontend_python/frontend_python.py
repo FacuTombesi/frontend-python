c50fd59f292c53e3438826bea23456d9b42a9e34
FacuTombesi <facundomtombesi@gmail.com>
2025-04-07 21:19:59 -0300

    python frontend basic

--- frontend_python/frontend_python.py
+++ frontend_python/frontend_python.py
@@ -3,12 +3,14 @@ from frontend_python.components.navbar import navbar
 from frontend_python.components.footer import footer
 from frontend_python.views.header.header import header
 from frontend_python.views.links.links import links
+import frontend_python.styles.styles as styles
 
 # Class that manages the states of the website
 class State(rx.State):
     pass
 
 # Function that renders the components of the page
+# CSS styles are applied like this and are written with _ instead of - as in traditional CSS
 def index() -> rx.Component:
     return rx.box(
         navbar(),
@@ -16,7 +18,7 @@ def index() -> rx.Component:
             rx.stack(
                 header(),
                 links(),
-                max_width="600px", # CSS styles are applied like this and are written with _ instead of - as in traditional CSS
+                max_width=styles.MAX_WIDTH,
                 width="100%",
                 direction="column",
                 align="center",
