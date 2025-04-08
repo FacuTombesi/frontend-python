ab3836b4b3342448b0065eebdb64fcd37b551726
FacuTombesi <facundomtombesi@gmail.com>
2025-04-07 21:27:05 -0300

    python frontend basic

--- frontend_python/styles/styles.py
+++ frontend_python/styles/styles.py
@@ -6,5 +6,5 @@ MAX_WIDTH = "600px"
 # Classes
 class Spacer(Enum):
   SMALL="0.5em"
-  DEFAULT="1em"
+  DEFAULT="1em" # Using em instead of px is a good practice for web development. "1em" is equal to the website's current font-size
   BIG="2em"
