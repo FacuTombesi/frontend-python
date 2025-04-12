ece04d163a98b74a0b777b0627ff42184d37f9fc
FacuTombesi <facundomtombesi@gmail.com>
2025-04-11 22:24:58 -0300

    python frontend basic

--- frontend_python/styles/styles.py
+++ frontend_python/styles/styles.py
@@ -10,6 +10,11 @@ class Size(Enum):
   DEFAULT="1em" # Using em instead of px is a good practice for web development. "1em" is equal to the website's current font-size
   BIG="2em"
 
+class Font_Size(Enum):
+  TITLE="24px"
+  SUBTITLE="18px"
+  BODY="16px"
+
 # Global Styles
 GLOBAL_STYLE = {
   rx.button: {
