27d85564b2e316d739ea1f56c472ec1ced365b0c
FacuTombesi <facundomtombesi@gmail.com>
2025-04-17 21:35:56 -0300

    python frontend basic

--- frontend_python/views/header/header.py
+++ frontend_python/views/header/header.py
@@ -4,7 +4,7 @@ from frontend_python.components.title import title
 def header() -> rx.Component:
   return rx.stack(
     rx.avatar(fallback="FT", color_scheme="crimson", size="8"),
-    title("MINI-PROJECTS"),
+    title("mini-PROJECTS"),
     rx.text("Welcome to the Mini-projects hub."),
     direction="column",
     align="center"
